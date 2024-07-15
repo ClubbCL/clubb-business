1c6debb1e485896af0b3d44bc738a2d2afac4514
Guillermo Puente <guillermo.ps09@gmail.com>
2024-07-15 18:26:23 -0400

    feat: added shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
@@ -38,6 +39,10 @@ export default function Home() {
           priority
         />
       </div>
+
+      <Button>
+        shadcn button
+      </Button>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
