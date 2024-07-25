7b5eaecd1e23a641e1004d1628751abe620f4467
Guillermo Puente <guillermo.ps09@gmail.com>
2024-07-25 16:15:23 -0400

    🚀 feat: added base auth

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,11 +1,43 @@
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { Session } from '@supabase/supabase-js';
+import { Button } from '@ui/button';
 import { supabase } from '@utils';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const AuthForm = () => {
+  const [session, setSession] = useState<Session | null>(null);
   const { t } = useTranslation();
 
-  // TODO: remove this console.log
-  console.log(supabase);
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-  return <div>AuthForm: {t('helloWorld')}</div>;
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} providers={[]} />;
+  }
+
+  const logOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <div>
+      AuthForm: {t('helloWorld')}
+      <div>
+        <Button onClick={logOut}>Log Out</Button>
+      </div>
+    </div>
+  );
 };
