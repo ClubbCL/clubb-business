1d8a2ba45254c790d845d2231478685013900fa1
Fernando Silanes <fernandosilanes@gmail.com>
2025-05-26 17:56:30 -0400

    Update ClubbLogo.tsx

--- src/components/ClubbLogo/ClubbLogo.tsx
+++ src/components/ClubbLogo/ClubbLogo.tsx
@@ -2,6 +2,6 @@ import { cn } from '@/lib/utils';
 
 export const ClubbLogo: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, ...divProps }) => (
   <div className={cn('font-recoleta-bold text-[#343250] text-[36px] flex items-center', className)} {...divProps}>
-    clubb 😎 <span className="font-paytone-one bg-[#ff2c78] text-white text-[10px] px-2 rounded-full ml-4">BETA</span>
+    clubb <span className="font-paytone-one bg-[#ff2c78] text-white text-[10px] px-2 rounded-full ml-4">BETA</span>
   </div>
 );
